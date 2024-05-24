d80fe9a75799eb2601176cac9094430989863c2b
Filipe Gabriel <filipegabriel11@hotmail.com>
2024-05-23 22:45:35 -0300

    cliente routing, module and component created

--- clientes-frontend/src/app/app.module.ts
+++ clientes-frontend/src/app/app.module.ts
@@ -5,6 +5,11 @@ import { AppComponent } from './app.component';
 import { TemplateModule } from './template/template.module';
 import { HomeComponent } from './home/home.component';
 import { FooterComponent } from './footer/footer.component';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [{
+  path: 'home', component: HomeComponent
+}]
 
 @NgModule({
   declarations: [
@@ -14,7 +19,8 @@ import { FooterComponent } from './footer/footer.component';
   ],
   imports: [
     BrowserModule,
-    TemplateModule
+    TemplateModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
