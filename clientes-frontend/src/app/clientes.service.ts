2bfd02d158bdbe39dccdcad62999516f46f314cf
Filipe Gabriel <filipegabriel11@hotmail.com>
2024-05-28 19:24:55 -0300

    Method post is running, and success alert implemented, added anottation CrossOrigin to sit url in backend

--- clientes-frontend/src/app/clientes.service.ts
+++ clientes-frontend/src/app/clientes.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Cliente } from './clientes/cliente';
 import { HttpClient } from '@angular/common/http'
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -9,6 +10,10 @@ export class ClientesService {
 
   constructor( private http: HttpClient ) {
 
+  }
+
+  salvar( cliente: Cliente): Observable<Cliente> {
+    return this.http.post<Cliente>('http://localhost:8081/api/clientes' , cliente);
   }
 
   getCliente() : Cliente {
