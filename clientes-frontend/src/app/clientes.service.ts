4e2be39cbecf8eaa8b58f91eef5b8079da9b3f0d
Filipe Gabriel <filipegabriel11@hotmail.com>
2024-06-02 20:38:15 -0300

    Registered customer listing component created

--- clientes-frontend/src/app/clientes.service.ts
+++ clientes-frontend/src/app/clientes.service.ts
@@ -15,11 +15,19 @@ export class ClientesService {
   salvar( cliente: Cliente): Observable<Cliente> {
     return this.http.post<Cliente>('http://localhost:8081/api/clientes' , cliente);
   }
-
-  getCliente() : Cliente {
-    let cliente: Cliente = new Cliente();
-    cliente.nome = 'Fulano de tal';
-    cliente.cpf = '123456';
-    return cliente;
+/*
+  getClientes(): Observable<Cliente[]>{
+    return this.http.get<Cliente[]>('');
   }
+*/
+
+  getClientes(): Cliente[]{
+    let cliente = new Cliente();
+    cliente.id = 1;
+    cliente.nome = 'Fulano';
+    cliente.dataCadastro = '18/04/2020';
+    cliente.cpf = '11201955432';
+    return [cliente];
+  }
+
 }
