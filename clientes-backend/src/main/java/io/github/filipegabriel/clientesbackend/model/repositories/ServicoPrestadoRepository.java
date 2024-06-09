4d4c839612f1d9d5872c72bfc0f81cafc9b4b384
Filipe Gabriel <filipegabriel11@hotmail.com>
2024-06-09 08:50:05 -0300

    Created the request to search for a customer by name

--- clientes-backend/src/main/java/io/github/filipegabriel/clientesbackend/model/repositories/ServicoPrestadoRepository.java
+++ clientes-backend/src/main/java/io/github/filipegabriel/clientesbackend/model/repositories/ServicoPrestadoRepository.java
@@ -1,9 +1,16 @@
 package io.github.filipegabriel.clientesbackend.model.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import io.github.filipegabriel.clientesbackend.model.entities.ServicoPrestado;
 
 public interface ServicoPrestadoRepository extends JpaRepository<ServicoPrestado, Integer>{
+	
+	@Query(" SELECT s FROM ServicoPrestado s JOIN s.cliente c WHERE UPPER( c.nome ) LIKE UPPER( :nome ) AND MONTH( s.data ) = :mes ")
+	List<ServicoPrestado> findByClienteAndMes(@Param("nome") String nome, @Param("mes") Integer mes);
 
 }
--- clientes-backend/src/main/java/io/github/filipegabriel/clientesbackend/rest/ServicoPrestadoController.java
+++ clientes-backend/src/main/java/io/github/filipegabriel/clientesbackend/rest/ServicoPrestadoController.java
@@ -2,11 +2,14 @@ package io.github.filipegabriel.clientesbackend.rest;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.util.List;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
@@ -32,9 +35,8 @@ public class ServicoPrestadoController {
 	@ResponseStatus(HttpStatus.CREATED)
 	public ServicoPrestado salvar(@RequestBody ServicoPrestadoDTO dto) {
 		LocalDate data = LocalDate.parse(dto.getData(), DateTimeFormatter.ofPattern("dd/MM/yyyy"));
-		Integer idCliente = dto.getIdCliente();
 		
-		Cliente cliente = clienteRepository.findById(idCliente).orElseThrow(() -> new ResponseStatusException(HttpStatus.BAD_REQUEST, "Cliente inexistente."));
+		Cliente cliente = clienteRepository.findById(dto.getIdCliente()).orElseThrow(() -> new ResponseStatusException(HttpStatus.BAD_REQUEST, "Cliente inexistente."));
 		
 		ServicoPrestado servicoPrestado = new ServicoPrestado();
 		servicoPrestado.setDescricao(dto.getDescricao());
@@ -42,6 +44,14 @@ public class ServicoPrestadoController {
 		servicoPrestado.setCliente(cliente);
 		servicoPrestado.setValor(bigDecimalConverter.converter(dto.getPreco()));
 		return servicoPrestadoRepository.save(servicoPrestado);
+	}
+	
+	@GetMapping
+	public List<ServicoPrestado> pesquisar(
+			@RequestParam(value="nome", required=false, defaultValue="") String nome, 
+			@RequestParam(value="mes", required=false) Integer mes
+	){
+		return servicoPrestadoRepository.findByClienteAndMes("%" + nome + "%", mes);
 	}
 	
 }
