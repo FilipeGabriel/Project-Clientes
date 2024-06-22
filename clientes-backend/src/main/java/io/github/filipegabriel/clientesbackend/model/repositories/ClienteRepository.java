fbec6949676c1168511ea6915fc075ccdf095e95
Filipe Gabriel <filipegabriel11@hotmail.com>
2024-06-21 21:06:25 -0300

    removed old api from project

--- clientes-backend/src/main/java/io/github/filipegabriel/clientesbackend/model/repositories/ClienteRepository.java
+++
@@ -1,9 +0,0 @@
-package io.github.filipegabriel.clientesbackend.model.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import io.github.filipegabriel.clientesbackend.model.entities.Cliente;
-
-public interface ClienteRepository extends JpaRepository<Cliente, Integer>{
-
-}
